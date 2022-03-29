c1d3237ae226bd1c70e279cc3316da90507a89a7
Roman Chuklinov <39238133+xjt85@users.noreply.github.com>
2022-03-29 12:33:52 +1000

    Update settings.py

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -28,9 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['localhost',
                  '127.0.0.1',
                  '[::1]',
-                 'testserver',
-                 'www.xjt.pythonanywhere.com',
-                 'xjt.pythonanywhere.com'
+                 'testserver'
                  ]
 
 
